0033d31c7f2a23186d3b143bfeaed3dea7e01122
Ben Do <bdo.appworkshop@gmail.com>
2024-07-21 17:53:20 -0500

    Added MealsRowView.

---
+++ Fetch Meals/View/MealsRowView.swift
@@ -0,0 +1,36 @@
+//
+//  MealsRowView.swift
+//  Fetch Meals
+//
+//  Created by Ben Do on 7/19/24.
+//
+
+import SwiftUI
+
+struct MealsRowView: View {
+    
+    @State var meal: Meals?
+    
+    var body: some View {
+        HStack {
+            if let imageUrl = URL(string: meal?.strMealThumb ?? "") {
+                AsyncImage(url: imageUrl)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                    .clipped()
+            }
+            Text(meal?.strMeal ?? "")
+                .font(.headline)
+                .foregroundStyle(.primary)
+                .lineLimit(2)
+                .multilineTextAlignment(.leading)
+            Spacer()
+        }
+        .padding(.vertical, 8)
+        .padding(.horizontal, 16)
+    }
+}
+
+#Preview {
+    MealsRowView()
+}
