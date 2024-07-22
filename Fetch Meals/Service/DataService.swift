32126a20444ba51d0540bd8dc909784080e51fcf
Ben Do <bdo.appworkshop@gmail.com>
2024-07-21 22:20:07 -0500

    Edited makeIngredientsAndMeasurements method.

--- Fetch Meals/Service/DataService.swift
+++ Fetch Meals/Service/DataService.swift
@@ -16,13 +16,21 @@ class DataService {
     func makeIngredientsAndMeasurements(detail: MealsDetail?) -> String {
         var ingredientsAndMeasurements: [String] = []
         
-        let mirror = Mirror(reflecting: detail!)
-        
         for index in 1...20 {
-            if let ingredient = mirror.children.first(where: { $0.label == "strIngredient\(index)" })?.value as? String, let measure = mirror.children.first(where: { $0.label == "strMeasure\(index)" })?.value as? String, !ingredient.isEmpty, !measure.isEmpty {
-                ingredientsAndMeasurements.append("\(ingredient): \(measure)")
+            let ingredientKey = "ingredient\(index)"
+            let measureKey = "measure\(index)"
+            
+            // Use Mirror to reflect on properties of Meal
+            let mirror = Mirror(reflecting: detail!)
+            
+            let ingredientValue = mirror.children.first { $0.label == ingredientKey }?.value as? String ?? ""
+            let measureValue = mirror.children.first { $0.label == measureKey }?.value as? String ?? ""
+            
+            if !ingredientValue.isEmpty && !measureValue.isEmpty {
+                ingredientsAndMeasurements.append("\(ingredientValue): \(measureValue)")
             }
         }
+        
         return ingredientsAndMeasurements.joined(separator: "\n")
     }
 }
