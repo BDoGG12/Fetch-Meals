4ef7b7a10128dd92bdae5f6c02b2141f0e851c44
Ben Do <bdo.appworkshop@gmail.com>
2024-07-22 13:08:51 -0500

    Passed in default value parameter Mirror.

--- Fetch Meals/Service/DataService.swift
+++ Fetch Meals/Service/DataService.swift
@@ -20,8 +20,10 @@ class DataService {
             let ingredientKey = "ingredient\(index)"
             let measureKey = "measure\(index)"
             
+            let defaultValue = MealsDetail(idMeal: "", meal: "", category: "", instructions: "", mealThumbnail: "", ingredient1: "", ingredient2: "", ingredient3: "", ingredient4: "", ingredient5: "", ingredient6: "", ingredient7: "", ingredient8: "", ingredient9: "", ingredient10: "", ingredient11: "", ingredient12: "", ingredient13: "", ingredient14: "", ingredient15: "", ingredient16: "", ingredient17: "", ingredient18: "", ingredient19: "", ingredient20: "", measure1: "", measure2: "", measure3: "", measure4: "", measure5: "", measure6: "", measure7: "", measure8: "", measure9: "", measure10: "", measure11: "", measure12: "", measure13: "", measure14: "", measure15: "", measure16: "", measure17: "", measure18: "", measure19: "", measure20: "")
+            
             // Use Mirror to reflect on properties of Meal
-            let mirror = Mirror(reflecting: detail!)
+            let mirror = Mirror(reflecting: detail ?? defaultValue)
             
             let ingredientValue = mirror.children.first { $0.label == ingredientKey }?.value as? String ?? ""
             let measureValue = mirror.children.first { $0.label == measureKey }?.value as? String ?? ""
